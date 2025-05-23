ca813a962e2c42f7a7a80679bcadb2ce104802bf
keysharvnl <keysha.putri0619@gmail.com>
2025-05-23 03:10:58 -0700

    Update fix lpk.py

--- fix lpk.py
+++ fix lpk.py
@@ -58,7 +58,7 @@ st.markdown(
     """
     <div class="selamat-datang">
         <h2 style='color: #ff8080; text-align:center;'>Selamat Datang di Aplikasi Menghitung Kadar Gula Dalam Jus Buah</h2>
-        <p style='color: #333333; text-align:justify;'>Aplikasi ini dirancang untuk membantu Anda menghitung jumlah kadar gula dalam berbagai jenis buah sebanyak 250mL. 
+        <p style='color: #333333; text-align:justify;'>Aplikasi ini dirancang untuk membantu Anda menghitung jumlah kadar gula dalam berbagai jenis buah sebanyak 250mL yang mengandung sekitar 225ml air. 
         Pilih jus buah yang anda ingin ketahui kadar gulanya dan aplikasi kami akan memberikan informasi tentang jumlah kadar gulanya.
         </p>
     </div>
@@ -71,7 +71,7 @@ st.markdown("<h1 style='color:#ff8080;'>🍓kalkulator kadar gula dalam jus buah
 
 # Data dan sidebar
 informasi_tambahan = {
-     'apel': {'manfaat': "Apel mengandung serat yang baik untuk pencernaan dan antioksidan yang dapat membantu menjaga kesehatan jantung", 'cara_memilih': "Pilih apel yang berwarna cerah, beratnya padat, dan tanpa memar. Hindari apel yang terlalu lembek"},
+    'apel': {'manfaat': "Apel mengandung serat yang baik untuk pencernaan dan antioksidan yang dapat membantu menjaga kesehatan jantung", 'cara_memilih': "Pilih apel yang berwarna cerah, beratnya padat, dan tanpa memar. Hindari apel yang terlalu lembek"},
     'pisang': {'manfaat': "Pisang kaya akan kalium yang baik untuk kesehatan jantung dan membantu menjaga tekanan darah.", 'cara_memilih': "Pilih pisang yang kulitnya tidak terlalu berwarna hijau dan tidak terlalu berwarna cokelat. Pilih yang masih dalam kondisi sedikit kehijauan"},
     'jeruk': {'manfaat': "Jeruk kaya akan vitamin C yang baik untuk sistem kekebalan tubuh dan mengandung antioksidan untuk kesehatan kulit", 'cara_memilih': "Pilih jeruk yang beratnya padat dan berwarna cerah. Hindari jeruk yang terlalu lembek atau memiliki bintik-bintik coklat"},
     'pir': {'manfaat': "Pir mengandung serat yang baik untuk pencernaan dan mengandung antioksidan yang membantu menjaga kesehatan tubuh", 'cara_memilih': "Pilih pir yang berwarna cerah dan padat. Hindari pir yang terlalu lembek atau memiliki bintik-bintik coklat"},
@@ -89,15 +89,6 @@ informasi_tambahan = {
     'blueberry': {'manfaat': "Blueberry kaya antioksidan, baik untuk otak, mata, dan kesehatan jantung", 'cara_memilih': "Pilih blueberry yang berwarna biru gelap, terasa kenyal dan tidak terlalu lembek, noda putih pada blueberry adalah tanda bahwa buah tersebut sudah terlalu lama disimpan"},
     'nanas': {'manfaat': "Nanas melancarkan pencernaan, anti-inflamasi dan penyembuhan luka, meningkatkan kekebalan tubuh, menjaga kesehatan jantung, menjaga kesehatan kulit", 'cara_memilih': "pilih warna kulit kuning keemasan, Aroma Harum manis di bagian pangkal, Tekstur Kulit sedikit lunak saat ditekan lembut, Daun mahkota Hijau segar dan mudah dicabut jika buah sudah matang, Berat nanas matang terasa berat, menandakan banyak air dan daging buah"},
     'markisa': {'manfaat': "Markisa baik untuk relaksasi, pencernaan, dan kulit karena kaya antioksidan", 'cara_memilih': "Markisa yang matang memiliki kulit yang sedikit keriput. Buah yang terlalu halus atau mulus mungkin belum matang. Pilih markisa yang terasa berat untuk ukurannya, karena ini menunjukkan bahwa buah tersebut mengandung banyak air dan rasa yang lebih manis. Markisa matang akan mengeluarkan aroma harum yang kuat"},
-        
-    
-
-
-
-
-
-
-
 }
 
 st.sidebar.title('Informasi Tambahan')
@@ -110,24 +101,24 @@ if buah_info and st.sidebar.checkbox('Tampilkan Informasi Tambahan'):
     st.sidebar.write(informasi_tambahan[buah_info]['cara_memilih'])
 
 kadargula_buah = {
-    'Jus apel': {'kadar gula': 24, 'vitamin': {'Vitamin A': '3%', 'Vitamin C': '14%'}},
-    'Jus pisang': {'kadar gula': 24, 'vitamin': {'Vitamin B6': '20%', 'Vitamin C': '14%'}},
-    'Jus jeruk': {'kadar gula': 21, 'vitamin': {'Vitamin C': '90%'}},
-    'Jus pir': {'kadar gula': 12, 'vitamin': {'Vitamin C': '7%'}},
-    'Jus strawberry': {'kadar gula': 13, 'vitamin': {'Vitamin C': '98%'}},
-    'Jus semangka': {'kadar gula': 9, 'vitamin': {'Vitamin A': '11%', 'Vitamin C': '13%'}},
-    'Jus mangga': {'kadar gula': 30, 'vitamin': {'Vitamin A': '25%', 'Vitamin C': '76%'}},
-    'Jus alpukat': {'kadar gula': 2, 'vitamin': {'Vitamin K': '26%', 'Vitamin E': '14%'}},
-    'Jus kiwi': {'kadar gula': 20, 'vitamin': {'Vitamin C': '112%', 'Vitamin K': '38%'}},
-    'Jus melon': {'kadar gula': 12, 'vitamin': {'Vitamin A': '12%', 'Vitamin C': '61%'}},
-    'Jus buah naga': {'kadar gula': 15, 'vitamin': {'Vitamin C': '9%', 'Vitamin B3': '8%'}},
-    'Jus sirsak': {'kadar gula': 12, 'vitamin': {'Vitamin C': '24%', 'Vitamin B6': '5%'}},
-    'Jus tomat': {'kadar gula': 7, 'vitamin': {'Vitamin C': '15%', 'Vitamin A': '28%'}},
-    'Jus pepaya': {'kadar gula': 36, 'vitamin': {'Vitamin C': '67%', 'Vitamin A': '19%'}},
-    'Jus lemon': {'kadar gula': 6, 'vitamin': {'Vitamin C': '59%'}},
-    'Jus blueberry': {'kadar gula': 35, 'vitamin': {'Vitamin C': '11%', 'Vitamin A': '1%'}},
-    'Jus nanas': {'kadar gula': 30, 'vitamin': {'Vitamin C': '80%', 'Vitamin B1': '7%'}},
-    'Jus markisa': {'kadar gula': 30, 'vitamin': {'Vitamin C': '33%', 'Vitamin A': '7%'}},
+    'alpukat': {'kadar gula': 2, 'vitamin': {'Vitamin K': '26%', 'Vitamin E': '14%'}},
+    'apel': {'kadar gula': 24, 'vitamin': {'Vitamin A': '3%', 'Vitamin C': '14%'}},
+    'blueberry': {'kadar gula': 35, 'vitamin': {'Vitamin C': '11%', 'Vitamin A': '1%'}},
+    'buah naga': {'kadar gula': 15, 'vitamin': {'Vitamin C': '9%', 'Vitamin B3': '8%'}},
+    'jeruk': {'kadar gula': 21, 'vitamin': {'Vitamin C': '90%'}},
+    'kiwi': {'kadar gula': 20, 'vitamin': {'Vitamin C': '112%', 'Vitamin K': '38%'}},
+    'lemon': {'kadar gula': 6, 'vitamin': {'Vitamin C': '59%'}},
+    'mangga': {'kadar gula': 30, 'vitamin': {'Vitamin A': '25%', 'Vitamin C': '76%'}},
+    'markisa': {'kadar gula': 30, 'vitamin': {'Vitamin C': '33%', 'Vitamin A': '7%'}},
+    'melon': {'kadar gula': 12, 'vitamin': {'Vitamin A': '12%', 'Vitamin C': '61%'}},
+    'nanas': {'kadar gula': 30, 'vitamin': {'Vitamin C': '80%', 'Vitamin B1': '7%'}},
+    'pepaya': {'kadar gula': 36, 'vitamin': {'Vitamin C': '67%', 'Vitamin A': '19%'}},
+    'pisang': {'kadar gula': 24, 'vitamin': {'Vitamin B6': '20%', 'Vitamin C': '14%'}},
+    'pir': {'kadar gula': 12, 'vitamin': {'Vitamin C': '7%'}},
+    'semangka': {'kadar gula': 9, 'vitamin': {'Vitamin A': '11%', 'Vitamin C': '13%'}},
+    'sirsak': {'kadar gula': 12, 'vitamin': {'Vitamin C': '24%', 'Vitamin B6': '5%'}},
+    'strawberry': {'kadar gula': 13, 'vitamin': {'Vitamin C': '98%'}},
+    'tomat': {'kadar gula': 7, 'vitamin': {'Vitamin C': '15%', 'Vitamin A': '28%'}},
 }
 
 # Mengubah urutan judul dan kalkulator
@@ -138,7 +129,7 @@ buah = st.selectbox('Pilih Buah', list(kadargula_buah.keys()), format_func=lambd
 
 if st.button('Hitung Kadar Gula'):
     kadargula_total = kadargula_buah[buah]['kadar gula']
-    st.success(f"Jumlah kadar gula dalam 250mL {buah.lower()} adalah: {kadargula_total} gram")
+    st.success(f"Jumlah kadar gula dalam 250mL yang mengandung 225ml air {buah.lower()} adalah: {kadargula_total} gram")
     st.write("Kandungan Vitamin:")
     for vitamin, nilai in kadargula_buah[buah]['vitamin'].items():
         st.write(f"- {vitamin}: {nilai}")
